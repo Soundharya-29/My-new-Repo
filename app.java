3f076498334575099adae866393e7e7100c6a90b
root <root@ip-172-31-24-51.ec2.internal>
2026-03-22 08:57:43 +0000

    data adding in

--- app.java
+++ app.java
@@ -12,12 +12,13 @@ public class EvenOdd {
 					            if(num % 2 == 0)
 							                System.out.println(num + " is even");
 						            else
-								                System.out.println(num + " is odd");
+								                System.out.println(num                                                                                      
 							        }java.util.Scanner
 
 
-//dev 2 code
+//dev 2 code 
 public class MultiplyTwoNumbers {
+
 
     public static void main(String[] args) {
 
@@ -29,3 +30,6 @@ public class MultiplyTwoNumbers {
         System.out.println("The product is: " + product);
     }
 }
+
+new code by dev 1
+
